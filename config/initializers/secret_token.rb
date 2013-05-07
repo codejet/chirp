66a3b0e2a663fdc646ff5eb3ee6de8129ee3b08a
Oliver Schmidt <oliver@Olivers-MacBook.local>
2013-05-07 22:35:42 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chirp::Application.config.secret_token = '12be60060af1a4f96f277c824a37a4e6f3f361989a8bf40ed2f9cd962328fec17592692689aac7f56fab911b0ea18f7c2d3a7009fb84597db747b83fab3cd542'
