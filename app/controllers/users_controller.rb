5edf67e12e64f411f5a894ea63a96e59f7993545
Oliver Schmidt <oliver@Olivers-MacBook.local>
2013-06-26 23:57:51 +0200

    part 9 - updating, showing, and deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy, :admin]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
+  before_filter :signed_in_user_filter, only: [:new, :create]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -17,4 +22,58 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def destroy
+    @user = User.find(params[:id])
+
+    if current_user?(@user)
+      flash[:error]  = "Deleting admin users is not allowed"
+    else
+      @user.destroy
+      flash[:notice] = "User successfully deleted"
+    end
+
+    redirect_to users_path
+  end
+  
+  def edit    
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+    
+    def signed_in_user_filter
+      if signed_in?
+        redirect_to root_path
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
