f960d7fdfb9994059a8e4769348cc9931ad46e82
Oliver Schmidt <oliver@Olivers-MacBook.local>
2013-07-28 15:13:50 +0200

    part 10 - user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,24 +16,40 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
     it { should_not have_selector 'title', text: '| Home' }
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
   end
-  
+
   describe "Help page" do
-    before { visit help_path } 
+    before { visit help_path }
     let(:heading)    { 'Help' }
     let(:page_title) { 'Help' }
 
     it_should_behave_like "all static pages"
   end
-  
+
   describe "About page" do
-    before { visit about_path } 
+    before { visit about_path }
     let(:heading)    { 'About Us' }
     let(:page_title) { 'About Us' }
 
     it_should_behave_like "all static pages"
   end
-  
+
   describe "Contact page" do
     before { visit contact_path }
     let(:heading)    { 'Contact' }
@@ -41,7 +57,7 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
   end
-  
+
   it "should have the right links on the layout" do
     visit root_path
     click_link "About"
@@ -56,5 +72,5 @@ describe "Static pages" do
     click_link "sample app"
     page.should have_selector 'h1', text: 'Sample App'
   end
-  
+
 end
